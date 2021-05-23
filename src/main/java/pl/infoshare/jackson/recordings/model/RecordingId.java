63310cf42bf92e9fa7485ac6227f5f84bc60f1f2
maciej.koziara <maciej.koziara@airhelp.com>
2021-05-23 18:52:49 +0200

    Solution for 2nd exercise

--- src/main/java/pl/infoshare/jackson/recordings/model/RecordingId.java
+++ src/main/java/pl/infoshare/jackson/recordings/model/RecordingId.java
@@ -1,9 +1,13 @@
 package pl.infoshare.jackson.recordings.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Value;
+import pl.infoshare.jackson.recordings.views.RecordingView;
 
 @Value
 public class RecordingId {
+    @JsonView(RecordingView.List.class)
     Integer id;
+    @JsonView(RecordingView.List.class)
     RecordingType type;
 }
