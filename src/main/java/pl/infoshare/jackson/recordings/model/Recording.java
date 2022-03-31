ab2d6ae3d35619b58b439fc6e2f8c5a6c0b6975a
maciej.koziara <maciej.koziara@dac.digital>
2022-03-31 19:50:54 +0200

    Improve 3rd solution

--- src/main/java/pl/infoshare/jackson/recordings/model/Recording.java
+++ src/main/java/pl/infoshare/jackson/recordings/model/Recording.java
@@ -1,7 +1,17 @@
 package pl.infoshare.jackson.recordings.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonView;
+import pl.infoshare.jackson.recordings.views.RecordingView;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "recordingType")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Song.class, name = "SONG"),
+        @JsonSubTypes.Type(value = Podcast.class, name = "PODCAST"),
+        @JsonSubTypes.Type(value = AudioBook.class, name = "AUDIOBOOK")
+})
 public interface Recording {
     @JsonIgnore
     Integer getId();
@@ -9,10 +19,11 @@ public interface Recording {
     @JsonIgnore
     RecordingType getType();
 
+    @JsonView(RecordingView.List.class)
     String getTitle();
-
     Recording withId(Integer id);
 
+    @JsonView(RecordingView.List.class)
     default RecordingId getExternalId() {
         return new RecordingId(getId(), getType());
     }
---
+++ src/main/java/pl/infoshare/jackson/recordings/views/RecordingView.java
@@ -0,0 +1,5 @@
+package pl.infoshare.jackson.recordings.views;
+
+public interface RecordingView {
+    class List {}
+}
